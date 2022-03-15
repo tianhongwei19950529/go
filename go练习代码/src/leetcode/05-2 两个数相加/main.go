8462563acecd5b9b3f4d284c71e93353bf338949
v_thwtian <v_thwtian@tencent.com>
2022-03-15 18:42:18 +0800

    new

---
+++ go练习代码/src/leetcode/05-2 两个数相加/main.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/*
+1. 定义一个链表,以及一个标识(判断下一位要不要+1)
+2. 两个链表直接对应位置可以直接相加
+3. 循环链表,取到当前的值,没有的话默认给0
+4. 每次都要把Next赋值给对应的结构体.
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) (head *ListNode) {
+	var tail *ListNode
+	nums := 0
+	for l1 != nil || l2 != nil {
+		n1, n2 := 0, 0
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		sum := n1 + n2 + nums
+		sum, nums = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+	if nums > 0 {
+		tail = &ListNode{Val: nums}
+		tail.Next = tail
+	}
+	return
+}
+
+func main() {
+	//lst := []int{}
+}
